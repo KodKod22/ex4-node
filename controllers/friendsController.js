e52f48476fd6c33c8e23bee5cfc1387c7fe21964
kodyAgin <kodyagin@gmail.com>
2024-07-11 16:14:57 +0300

    adding the ability to create random access key when adding friend

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -1,15 +1,19 @@
 
 exports.friendsController = {
     async addFriend(req,res){
-        //if !req.friend_name || !friend pasword thorw error
+        
+        if(!req.friend_name || !req.friend_password){
+            console.error('input null',req.friend_name,req.friend_password);
+            res.status(400).send('input is broken');
+        }
+        
         const token = crypto.randomUUID();
+        console.log(token);
         const {dbConnection} = require('../dbConnection');
         const connection = await dbConnection.createConnection();
         const {body} = req;
         await connection.execute(`INSERT INTO tbl_55_friends (friend_name,friend_password,friend_access_key) VALUES("${body.friend_name}", "${body.friend_password}","${token}")`);
         connection.end();
-        res.send(true);
+        res.send(token);
     }
 }
-/*
-1) */
